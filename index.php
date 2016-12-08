bfd53390839f404066514bc86a8922b8c0035c61
yvescyuzuz <yves.cyuzuzo@rexvirt.com>
2016-12-08 18:27:09 +0900

    initial files added

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart delivery</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="bootstrap337/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<script src="bootstrap337/js/jquery.min.js"></script>
+  	<script src="bootstrap337/js/bootstrap.min.js"></script> 
+</head>
+<body>
+	<div class="container-fluid" style="background-color: purple">
+		<div class="container">
+			<div class="row" >
+				<img src="images/logo.png" class="img-rounded" alt="Smart delivery logo" style="margin-left: 12px">
+			</div>
+			<div class="row" style="margin-top: 30px; margin-bottom:40px">
+				<div class="col-md-6">
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="videos/SDScenario.mp4"></iframe>
+					</div>	
+				</div>
+				<div class="col-md-6 text-center">
+					<h2 style="color: white"> SMART Delivery</h2>
+					<h5 style="color: white">Stay Tuned</h5>
+					<h4 style="text-align: left; color: white; margin-top: 40px; margin-left: 100px">
+						<ul style="list-style-type: square; ">
+							<li>Get your item delivered on time</li>
+							<li>Make money with us</li>
+						</ul>
+					</h4>
+					<form>
+						<div class="col-md-10" style="margin-top: 40px; margin-left: 40px" >
+							<div class="input-group input-group-lg">
+								<input type="text" class="form-control" placeholder="Enter your email...">
+								<div class="input-group-btn">
+									<button class="btn btn-success" type="submit">Subscribe</button>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid" style="">
+		<div class="container">
+			<div class="row text-center">
+				<div class="col-md-4" style="margin-top: 50px">
+					<i class="fa fa-circle-thin" aria-hidden="true" style="font-size: 200px"></i>
+				</div>
+				<div class="col-md-4">
+					<i class="fa fa-circle-thin" aria-hidden="true" style="font-size: 300px"></i>
+				</div>
+				<div class="col-md-4" style="margin-top: 50px">
+					<i class="fa fa-circle-thin" aria-hidden="true" style="font-size:200px"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
